c372e9af272615995dd7b380cdc62c5232f8be5e
Peter G <peterjonathang@gmail.com>
2025-01-24 21:18:55 -0800

    add todo

--- playground/playground.ts
+++ playground/playground.ts
@@ -1,3 +1,3 @@
-// playground.ts
+// TODO: configure some way to get the local typescript environment be what is used by this file
 const message: string = "Hello, TypeScript!";
 console.log(message);
